e23891909616bf9fba1958dc3819c8a89f4aa427
Mehedi Hasan <mehedi.hasan4846@gmail.com>
2021-12-02 03:18:05 +0600

    Completed the authentication system

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,33 +1,38 @@
 // external imports
 const express = require("express");
+const { check } = require("express-validator");
 
 // internal imports
-const avatarUpload = require("../middlewares/user/avatarUpload");
 const {
   getUsers,
   addUser,
   removeUser,
-} = require("../controller/userController");
+} = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
 const {
   addUserValidators,
   addUserValidationHandler,
-} = require("../middlewares/user/userValidators");
+} = require("../middlewares/users/userValidators");
+
+const { checkLogin } = require("../middlewares/common/checkLogin");
 
 const router = express.Router();
 
-// user page
-router.get("/", decorateHtmlResponse("Users"), getUsers);
+// users page
+router.get("/", decorateHtmlResponse("Users"), checkLogin, getUsers);
 
-// add new user
+// add user
 router.post(
   "/",
+  checkLogin,
   avatarUpload,
   addUserValidators,
   addUserValidationHandler,
   addUser
 );
 
+// remove user
 router.delete("/:id", removeUser);
 
 module.exports = router;
